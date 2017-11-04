a054dd7f82301805f8dfdd6fe19eef8b39fd2b00
Haris Imamović <himamovic1@etf.unsa.ba>
2017-11-04 19:50:06 +0100

    Enabled the server to be available from the local network

--- Python/ETF-Competition/run_app.py
+++ Python/ETF-Competition/run_app.py
@@ -1,15 +1,32 @@
 from flask import Flask
 
 from competition.controllers.auth.views import Auth
+from competition.controllers.public.views import Public
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='competition/templates')
 
 
 @app.route('/')
-def hello_world():
-    auth = Auth()
-    auth.get()
+def index():
+    public = Public()
+    return public.index()
+
+
+@app.route('/home')
+def home():
+    public = Public()
+    return public.home()
 
 
 if __name__ == '__main__':
-    app.run()
+
+    # Print current local IP address of the server
+    import socket
+    print('{}\nLocal server IP address: {}\n{}'.format(
+        '-' * 50,
+        socket.gethostbyname(socket.gethostname()),
+        '-' * 50)
+    )
+
+    # Run the server
+    app.run(host='0.0.0.0')
